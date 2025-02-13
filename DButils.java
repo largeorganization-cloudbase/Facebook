a2feaf33b54b9da14cb8b59c5d91024217506116
mirzanaveedbaig <112803006+mirzanaveedbaig@users.noreply.github.com>
2025-02-13 02:14:07 -0800

    Update again DButils.java

--- DButils.java
+++ DButils.java
@@ -3,4 +3,5 @@ this is devlopment branch...
 this java file named dbutils files
 
 this is upatating
-this line from remote repo
+this line from remote repo for fetch command
+  this line for pull command
